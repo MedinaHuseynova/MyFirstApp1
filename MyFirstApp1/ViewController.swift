7c54c79faa314fde41b4621d9912a59606160b3f
MedinaHuseynova <131304421+MedinaHuseynova@users.noreply.github.com>
2023-08-31 01:21:45 +0400

    Initial Commit

---
+++ MyFirstApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp1
+//
+//  Created by Medina Huseynova on 31.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
